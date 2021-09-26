fa67dd0a4b70e8116afbf506ceca584a0a2d6f0e
Dave Hobson <davehobson90@gmail.com>
2021-09-25 20:47:38 -0400

    Added the weatrherstack apikey+excluded from git

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-//Added a comment to test a stage -> commit -> Push
-//Added a second comment line
-console.log('hello World')
+//Imports
+const fs = require('fs');
+
+//variables
+let jsonData;
+let appSettings;
+
+//read app settings from settings.json. This file is intentionally excluded from git
+try{
+    jsonData = fs.readFileSync('appsettings.json');
+}
+catch(e){
+    console.log(e)
+    console.error("ERROR:"+e.errno+" Failed to open "+e.path+" please create it with the following format and store your weatherstack.com api key:\n\
+    {\n\
+        apikey:1234567689\n\
+    \}\n")
+}
+
+appSettings = JSON.parse(jsonData);
+
+console.log(appSettings.apikey);
