1aae0d2c031d621d1ee89896c5ac0c2084186347
Dave Hobson <davehobson90@gmail.com>
2021-09-25 22:16:11 -0400

    Revert "Added http call to weatherstack.com"
    This reverts commit 7d5a54d345cf2e196654af4b264cbcce85fa8801.

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
 //Imports
 const fs = require('fs');
-const request = require('request');
-
-//Constants
-const baseUrl="http://api.weatherstack.com/";
-const mtlCoords="45.5017,-73.5673";
 
 //variables
 let jsonData;
 let appSettings;
-let apiCallString;
 
 //read app settings from settings.json. This file is intentionally excluded from git
 try{
     jsonData = fs.readFileSync('appsettings.json');
-    appSettings = JSON.parse(jsonData);
 }
 catch(e){
     console.log(e)
     console.error("ERROR:"+e.errno+" Failed to open "+e.path+" please create it with the following format and store your weatherstack.com api key:\n\
     {\n\
-        apikey:12345676890\n\
+        apikey:1234567689\n\
     \}\n")
 }
 
-//build our api call string for weatherstack in the format:
-//http://api.weatherstack.com/current?access_key=1234567890&query=45.5017,-73.5673
-apiCallString = baseUrl+"current?access_key="+appSettings.apikey+"&query="+mtlCoords;
-
-//output http call to console
-console.log("Api Call: "+apiCallString);
+appSettings = JSON.parse(jsonData);
 
 //make our http call to weatherstack.com
 request(apiCallString, {json:true}, (error, response, body) =>{
