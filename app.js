0464ffe102467a6851babc23b0b0707d2d1b7edb
Dave Hobson <davehobson90@gmail.com>
2021-09-28 20:57:46 -0400

    Swapped my appsettings.json api key to a .env var

--- app.js
+++ app.js
@@ -2,30 +2,21 @@
 const fs = require('fs');
 const forcast = require('./utils/forcast')
 
+//load .env enviroment variables
+require('dotenv').config();
+
 //Constants
 const mtlCoords="45.5017,-73.5673";
-const appSettingsFileName = "appsettings.json";
 
-//variables
-let jsonData;
-let appSettings;
-
-//read app settings from settings.json. 
-//This file is intentionally excluded from git to hide our weatherstack.com api key
-try{
-    jsonData = fs.readFileSync(appSettingsFileName);
-    appSettings = JSON.parse(jsonData);
+if(process.env.API_KEY){
+  //get our forcast data from the weatherstack api
+  forcast(mtlCoords, process.env.API_KEY, (error, data) => {
+    console.log('Error',error)
+    console.log('data',data)
+  })
 }
-catch(e){
-    console.error(`\FATAL ERROR: Failed to open ${e.path} please create it with the following format and store your weatherstack.com api key\n\
-    ${process.cwd()}\\${appSettingsFileName}\n\
-    {\n\
-        \"apikey\":\"12345676890\"\n\
-    \}\n`)
-    process.exit(1)
+else{
+  console.error(`\FATAL ERROR: Failed to find the API_KEY in our .env file  please create it with the following format and store your weatherstack.com api key\n\
+    #${process.cwd()}\\.env\n\
+    API_KEY=12345676890`)
 }
-
-forcast(mtlCoords, appSettings.apikey, (error, data) => {
-  console.log('Error',error)
-  console.log('data',data)
-})
